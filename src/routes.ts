9d1e8bcedbd9b01e445ee7433e02e20ee89a69e9
Eliabel Barreto <67025189+ebarretodev@users.noreply.github.com>
2021-12-04 22:36:35 -0300

    Creating routes for controller from user.

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,8 @@
 import { Router, Request, Response } from "express"
 
 //Import Middlewares
-
+const AuthController = require('./controllers/AuthController')
+const UserController = require('./controllers/UserController')
 
 //Routes definition
 const router = Router()
@@ -10,5 +11,11 @@ router.get('/', (req: Request, res: Response)=>{
     res.status(200)
     res.json({message: 'Welcome to API.v1 to CarWallet Simulator.'})
 })
+
+router.get('/user/me', UserController.info)
+router.put('/user/me', UserController.editAction)
+router.post('/user/signin', AuthController.signin)
+router.post('/user/signup', AuthController.signup)
+
 
 module.exports = router
