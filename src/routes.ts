31c4113046b22b29ff22bb5fc0cc27fde46b6b11
Eliabel Barreto <67025189+ebarretodev@users.noreply.github.com>
2021-12-13 01:04:50 -0300

    correction on routes for usage, like error handler, bad requests, and endpoint definitions

--- src/routes.ts
+++ src/routes.ts
@@ -4,23 +4,32 @@ import { Router, Request, Response } from "express"
 //Import Middlewares
 const AuthController = require('./controllers/AuthController')
 const UserController = require('./controllers/UserController')
-const StoreController = require('./controllers/StoreController')
+const NpcController = require('./controllers/NpcController')
+const IotaController = require('./controllers/IotaController')
+import { privateRoute } from "./config/passport"
 
 //Routes definition
 const router = Router()
+
+//Welcome message
 router.get('/', (req: Request, res: Response)=>{
     res.status(200)
     res.json({message: 'Welcome to API.v1 to CarWallet Simulator.'})
 })
 
-router.get('/user/me', UserController.info)
-router.put('/user/me', UserController.editAction)
 router.post('/user/signin', AuthController.signin)
 router.post('/user/signup', AuthController.signup)
 
-router.get('/stores/list', StoreController.list)
+router.get('/user/me', UserController.info)
+router.put('/user/me', UserController.editAction)
+router.delete('/user/me', UserController.deleteMe)
 
+router.get('/npc/list', NpcController.list)
+router.get('/npc/listCategories', NpcController.listCategories)
+router.get('/npc/:id/listServices', NpcController.listServices)
+router.post('/npc/:id/:service', NpcController.useService)
 
+router.post('/iota/buy', IotaController.buy)
 
 
 module.exports = router
--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ import { Request, Response, ErrorRequestHandler } from 'express'
 //DB connection
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
 
 mongoose.Promise = global.Promise
@@ -31,6 +31,19 @@ server.use(express.json())
 server.use(express.urlencoded({extended:true}))
 server.use(passport.initialize())
 server.use('/', routes)
+
+//Case not found
+server.use((req: Request, res: Response) => {
+    res.status(404)
+    res.json({ error: 'Not found endpoint' })
+})
+
+//Case occurs error on request
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    res.status(err.status ? err.status : 400)
+    res.json({error: err.message ? err.message : 'Error occurs.'})
+}
+server.use(errorHandler);
 
 //Running server
 server.listen(process.env.PORT, ()=>{
